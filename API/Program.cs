2e67345c91a32e116e452e2fc6289e6598f3e4eb
92061 <92061@studmail.htw-aalen.de>
2026-01-29 14:32:35 +0100

    Create openapi doc n build

--- API/Program.cs
+++ API/Program.cs
@@ -5,11 +5,6 @@ using Microsoft.AspNetCore.SignalR;
 using PhotographyOfMovingObjects;
 using Project;
 
-//Initialize Static classes....
-Console.WriteLine($"Trigger GPIO Pin: {Trigger.PinNumber}");
-Console.WriteLine($"Flash GPIO Pin: {Flash.PinNumber}");
-Console.WriteLine($"Delay Camera: {Photography.DelayCamera} Delay Flash: {Photography.DelayFlash}");
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,11 +15,7 @@ builder.Services.AddControllers()
         opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(opts =>
-{
-    opts.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
-});
+builder.Services.AddOpenApi();
 
 builder.Services.AddCors(options =>
 {
@@ -47,9 +38,11 @@ WebApplication app = builder.Build();
 
 app.UseHttpsRedirection();
 
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
+app.MapOpenApi();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+});
 
 app.MapControllers();
 
@@ -58,30 +51,43 @@ app.UseCors();
 app.UseHttpLogging();
 
 app.MapHub<NotificationHub>("/notifications");
-Camera.PictureTaken += async () =>
+
+try
 {
-    if (app.Services.GetService<IHubContext<NotificationHub>>() is not { } hub)
-        return;
-    await hub.Clients.All.SendAsync("snap");
-};
-Camera.PictureReady += async () =>
+    //Initialize Static classes....
+    Console.WriteLine($"Trigger GPIO Pin: {Trigger.PinNumber}");
+    Console.WriteLine($"Flash GPIO Pin: {Flash.PinNumber}");
+    Console.WriteLine($"Delay Camera: {Photography.DelayCamera} Delay Flash: {Photography.DelayFlash}");
+
+    Camera.PictureTaken += async () =>
+    {
+        if (app.Services.GetService<IHubContext<NotificationHub>>() is not { } hub)
+            return;
+        await hub.Clients.All.SendAsync("snap");
+    };
+    Camera.PictureReady += async () =>
+    {
+        if (app.Services.GetService<IHubContext<NotificationHub>>() is not { } hub)
+            return;
+        await hub.Clients.All.SendAsync("picture");
+    };
+    Trigger.Triggered += async type =>
+    {
+        if (app.Services.GetService<IHubContext<NotificationHub>>() is not { } hub)
+            return;
+        await hub.Clients.All.SendAsync("trigger", type);
+    };
+    Flash.Triggered += async () =>
+    {
+        if (app.Services.GetService<IHubContext<NotificationHub>>() is not { } hub)
+            return;
+        await hub.Clients.All.SendAsync("flash");
+    };
+}
+catch (Exception)
 {
-    if (app.Services.GetService<IHubContext<NotificationHub>>() is not { } hub)
-        return;
-    await hub.Clients.All.SendAsync("picture");
-};
-Trigger.Triggered += async type =>
-{
-    if (app.Services.GetService<IHubContext<NotificationHub>>() is not { } hub)
-        return;
-    await hub.Clients.All.SendAsync("trigger", type);
-};
-Flash.Triggered += async () => 
-{
-    if (app.Services.GetService<IHubContext<NotificationHub>>() is not { } hub)
-        return;
-    await hub.Clients.All.SendAsync("flash");
-};
+    Console.WriteLine("Failed starting dependencies.");
+}
 
 app.Run();
  
