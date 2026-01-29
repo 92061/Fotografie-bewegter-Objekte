f14d5937dab6eba23912f53b2f0385296f23971e
92061 <92061@studmail.htw-aalen.de>
2026-01-29 07:30:25 +0100

    Add GPIO Flash

---
+++ FlashTrigger/Flash.cs
@@ -0,0 +1,35 @@
+﻿using System.Device.Gpio;
+
+namespace FlashTrigger;
+
+public static class Flash
+{
+    private static readonly GpioController GpioController = new();
+    private const int DefaultFlashPinNumber = 18; 
+    private static GpioPin _flashPin;
+    private const int FlashHighTimeoutMs = 50;
+
+    static Flash()
+    {
+        _flashPin = GpioController.OpenPin(DefaultFlashPinNumber, PinMode.Output);
+    }
+
+    public static void SetFlashPin(int pinNumber)
+    {
+        _flashPin.Close();
+        _flashPin.Dispose();
+        _flashPin = GpioController.OpenPin(pinNumber, PinMode.Output);
+    }
+
+    public static Task Trigger()
+    {
+        return new (TriggerInternal);
+    }
+
+    private static void TriggerInternal()
+    {
+        _flashPin.Write(PinValue.High);
+        Thread.Sleep(FlashHighTimeoutMs);
+        _flashPin.Write(PinValue.Low);
+    }
+}
