4b9d1033188a8a9f55edc2e5459e17392c3eef47
92061 <92061@studmail.htw-aalen.de>
2026-01-29 07:30:25 +0100

    Initial Commit

---
+++ API/Controllers/ConfigurationController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ConfigurationController : ControllerBase
+{
+}
---
+++ Tests/CameraTests.cs
@@ -0,0 +1,26 @@
+﻿using CameraTrigger;
+
+namespace Tests;
+
+public class CameraTests
+{
+    [Fact]
+    public void TestAdbBridge()
+    {
+        Assert.True(Camera.Connect());
+    }
+
+    [Fact]
+    public void TestStartCamera()
+    {
+        Camera.Connect();
+        Assert.True(Camera.StartCameraApp());
+    }
+
+    [Fact]
+    public void TestTakingPicture()
+    {
+        Camera.Connect();
+        Assert.True(Camera.TakePicture());
+    }
+}
