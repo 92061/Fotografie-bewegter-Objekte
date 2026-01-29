3f304bd940e2281445553f7fc9182751da022e61
92061 <92061@studmail.htw-aalen.de>
2026-01-29 07:30:25 +0100

    Add Swagger

--- API/Program.cs
+++ API/Program.cs
@@ -9,14 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<Photography>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
