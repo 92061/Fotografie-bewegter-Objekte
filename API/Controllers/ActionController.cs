7c962e7875ab52f5b20e0ed08df7fad05d1e8e2d
92061 <92061@studmail.htw-aalen.de>
2026-01-29 07:30:25 +0100

    Swashbuckle and XML Documentation of API

--- API/Controllers/ActionController.cs
+++ API/Controllers/ActionController.cs
@@ -6,7 +6,14 @@ namespace Project.Controllers;
 [Route("[controller]")]
 public class ActionController : ControllerBase
 {
+    /// <summary>
+    /// Triggers the flash.
+    /// </summary>
+    /// <response code="200">Flash triggered</response>
+    /// <response code="500">Task failed!</response>
     [HttpPost("Flash")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public IActionResult Flash()
     {
         Task flash = FlashTrigger.Flash.Trigger(TimeSpan.Zero);
@@ -18,7 +25,14 @@ public class ActionController : ControllerBase
             return new StatusCodeResult(StatusCodes.Status500InternalServerError);
     }
 
+    /// <summary>
+    /// Takes a photo.
+    /// </summary>
+    /// <response code="200">Photo taken</response>
+    /// <response code="500">Task failed!</response>
     [HttpPost("Photo")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public IActionResult TakePhoto()
     {
         Task<bool> takePhoto = CameraTrigger.Camera.TakePicture(TimeSpan.Zero);
