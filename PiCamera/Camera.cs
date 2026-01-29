357b1321eb6297a98790f0af7daf29d9a32d840c
92061 <92061@studmail.htw-aalen.de>
2026-01-29 07:30:26 +0100

    Comments for PiCamera.Camera

--- PiCamera/Camera.cs
+++ PiCamera/Camera.cs
@@ -6,17 +6,28 @@ namespace PiCamera;
 
 public class Camera : IDisposable
 {
-    private readonly Process _rpiCamProc = new ();
+    private readonly Process _rpiCamProc;
     private readonly RpicamArgs _args;
     
+    /// <summary>
+    /// Gets invoked when the signal to capture a picture has been sent
+    /// </summary>
     public event CameraEvent? PictureTaken;
+    /// <summary>
+    /// Gets invoked when the Picture has been taken
+    /// </summary>
     public event CameraEvent? PictureReady;
     public delegate void CameraEvent();
     
+    /// <summary>
+    /// Create a new process of the specified app with the provided arguments-list
+    /// </summary>
+    /// <exception cref="Exception">Camera-app could not be started.</exception>
     public Camera(RpiCameraApp app, RpicamArgs args)
     {
-        this._args = args;
+        this._rpiCamProc = new Process();
         _rpiCamProc.Exited += RpiCamProcOnExited;
+        this._args = args;
         
         ProcessStartInfo cameraProcessInfo = new(app.AsString(), args.GetArgsString)
         {
@@ -26,69 +37,86 @@ public class Camera : IDisposable
         _rpiCamProc.StartInfo = cameraProcessInfo;
         
         Console.WriteLine($"{cameraProcessInfo.FileName} {cameraProcessInfo.Arguments}");
-        if(!_rpiCamProc.Start())
-            throw new Exception("Could not start Camera process");
-        else
+        if (!_rpiCamProc.Start())
         {
-            if(args.Output is Output.Stream)
-                _copyDataThread.Start(this);
-            Console.WriteLine($"PID: {_rpiCamProc.Id}");
-            Thread.Sleep(1000); //Startup delay
+            throw new Exception("Could not start Camera process");
         }
+        
+        if(args.Output is Output.Stream)
+            _copyDataThread.Start(this); // Start the thread that copies the data written to stdout if output type is Stream
+        
+        Console.WriteLine($"PID: {_rpiCamProc.Id}");
+        Thread.Sleep(500); //Startup delay
     }
 
+    /// <summary>
+    /// Index of the latest picture (used for file %d)
+    /// </summary>
     private uint _newPictureIndex = 0;
+    /// <summary>
+    /// Data of the lastest picture, if output-mode stream is used
+    /// </summary>
     private readonly MemoryStream _lastPicture = new();
+    /// <summary>
+    /// If output-mode is Stream, copy data from stdout to _lastPicture
+    /// </summary>
     private readonly Thread _copyDataThread = new (o =>
     {
         Camera? c = o as Camera;
         c?._rpiCamProc.StandardOutput.BaseStream.CopyTo(c._lastPicture);
     });
 
-    public bool TakePicture()
+    /// <summary>
+    /// Take a picture. <br />
+    /// Sends the signal to the camera-process to take a picture.
+    /// </summary>
+    /// <exception cref="Exception">Signal could not be sent.</exception>
+    public void TakePicture()
     {
+        // If output-mode is Stream, reset the Stream of the latest picture
         if (_args.Output is Output.Stream)
         {
             _lastPicture.Position = 0;
             _lastPicture.SetLength(0);
         }
 
-        bool ret;
+        // Lock the process, so separate threads can not try and take more than one picture at a time
         lock (_rpiCamProc)
         {
             // https://www.raspberrypi.com/documentation/computers/camera_software.html#signal
-            ret = _rpiCamProc.SendSignal(Signum.SIGUSR1);
+            if (!_rpiCamProc.SendSignal(Signum.SIGUSR1))
+                throw new Exception("Failed sending Signal to caputre picture");
+            
             PictureTaken?.Invoke();
             _newPictureIndex++;
         
+            // Wait for picture to be ready (taken and saved)
             if(_args.Output is Output.Stream)
-                Thread.Sleep(1000);
+                Thread.Sleep(1000); // TODO: Calculate or get the correct value
             else if (_args.Output is Output.File)
             {
                 while(!File.Exists(LatestFilePath) || !UnixHelper.IsWritten(LatestFilePath))
                     Thread.Sleep(10);
             }
             else
-                Thread.Sleep(100);
+                Thread.Sleep(100); // TODO: Calculate or get the correct value
             
             PictureReady?.Invoke();
         }
-        
-        return ret;
     }
 
+    /// <summary>
+    /// Get the latest picture that has been taken
+    /// </summary>
     public byte[] GetPicture()
     {
         if (_args.Output is Output.File)
         {
-            lock (LatestFilePath)
-            {
-                Console.WriteLine($"Getting file {LatestFilePath}");
-                if (File.Exists(LatestFilePath))
-                    return File.ReadAllBytes(LatestFilePath);
-                else
-                    Console.WriteLine($"File does not exist {LatestFilePath}");
-            }
+            Console.WriteLine($"Getting file {LatestFilePath}");
+            if (File.Exists(LatestFilePath))
+                return File.ReadAllBytes(LatestFilePath);
+            else
+                Console.WriteLine($"File does not exist {LatestFilePath}");
         }
 
         if (_args.Output is Output.Stream)
@@ -97,12 +125,20 @@ public class Camera : IDisposable
         return [];
     }
 
+    /// <summary>
+    /// Filename of the latest picture, if output-mode is File
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
     private string LatestFilePath => _args.OutputAdditional is { Length: > 0 } filePath
         ? filePath.Replace("%d", $"{_newPictureIndex - 1}")
         : throw new ArgumentException("Missing additional argument for File-output");
 
+    /// <summary>
+    /// If Camera-Process exits, end this program
+    /// </summary>
     private void RpiCamProcOnExited(object? sender, EventArgs e)
     {
+        this.Dispose();
         Environment.Exit(_rpiCamProc.ExitCode);
     }
     
@@ -116,8 +152,17 @@ public class Camera : IDisposable
     }
 }
 
+/// <summary>
+/// Helperclass to send Signals to a POSIX-Process
+/// </summary>
 internal static class UnixHelper
 {
+    /// <summary>
+    /// Send Signal to Process
+    /// </summary>
+    /// <param name="process"></param>
+    /// <param name="signal"></param>
+    /// <returns>true if signal was sent</returns>
     public static bool SendSignal(this Process process, Signum signal)
     {
         if (Syscall.kill(process.Id, signal) == 0)
