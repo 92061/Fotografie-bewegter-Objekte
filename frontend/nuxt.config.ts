aecafa31d0d303f2a874522ac79c80f2a2e7292e
92061 <92061@studmail.htw-aalen.de>
2026-01-29 07:30:26 +0100

    Remove extraneous data Add frontend boilerplate

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  devServer: {
+    host: '127.0.0.1'
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
