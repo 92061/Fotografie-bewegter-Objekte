06517aaecf3f7c3b347934f375540610cad530da
92061 <92061@studmail.htw-aalen.de>
2026-01-29 07:30:26 +0100

    Add openapi-ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,29 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-open-fetch"],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
-    '/': { prerender: true }
+    "/": { prerender: true },
   },
 
   devServer: {
-    host: '127.0.0.1'
+    host: "127.0.0.1",
   },
 
-  compatibilityDate: '2025-01-15',
+  compatibilityDate: "2025-01-15",
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-})
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+});
