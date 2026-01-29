5bde946b5dc35bb15e59c7387386f8893e01f46f
92061 <92061@studmail.htw-aalen.de>
2026-01-29 07:30:25 +0100

    Gpio Trigger

---
+++ ImpulseReader/Trigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Device.Gpio;
+
+namespace ImpulseReader;
+
+/*
+ * https://learn.microsoft.com/en-us/dotnet/iot/tutorials/gpio-input
+ */
+
+public static class Trigger
+{
+    private static readonly GpioController GpioController = new();
+    private const int DefaultTriggerPinNumber = 21; 
+    private static GpioPin _triggerPin;
+
+    public static event TriggeredEvent? Triggered;
+    public delegate void TriggeredEvent(PinEventTypes type);
+    
+    static Trigger()
+    {
+        _triggerPin = GpioController.OpenPin(DefaultTriggerPinNumber, PinMode.InputPullUp);
+        GpioController.RegisterCallbackForPinValueChangedEvent(DefaultTriggerPinNumber, 
+            PinEventTypes.Rising | PinEventTypes.Falling,
+            OnPinValueChanged);
+    }
+    
+    public static void SetTriggerPin(int pinNumber, PinMode mode = PinMode.InputPullUp)
+    {
+        _triggerPin.Close();
+        _triggerPin.Dispose();
+        GpioController.UnregisterCallbackForPinValueChangedEvent(_triggerPin.PinNumber, OnPinValueChanged);
+        _triggerPin = GpioController.OpenPin(pinNumber, mode);
+        GpioController.RegisterCallbackForPinValueChangedEvent(DefaultTriggerPinNumber, 
+            PinEventTypes.Rising | PinEventTypes.Falling,
+            OnPinValueChanged);
+    }
+
+    private static void OnPinValueChanged(object sender, PinValueChangedEventArgs e)
+    {
+        Triggered?.Invoke(e.ChangeType);
+    }
+}
